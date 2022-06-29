8b8defc440067d12334a37215cc17395fbcf4e14
luis <luishsilva09@gmail.com>
2022-06-29 17:51:43 -0300

    validar dados de cadastro joi

--- app.js
+++ app.js
@@ -16,8 +16,28 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/cadastro", (req, res) => {
-  res.send("foii");
+const cadastroSchema = joi.object({
+  name: joi.string().required(),
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+  repeat_password: joi.ref("password"),
 });
+
+app.post("/cadastro", async (req, res) => {
+  try {
+    const dados = req.body;
+    const { error } = cadastroSchema.validate(dados, { abortEarly: false });
+    if (error) {
+      return res.sendStatus(422);
+    }
+  } catch {
+    res.sendStatus(500);
+  }
+});
+
+app.post("/entrada", (req, res) => {});
+
+app.post("/saida", (req, res) => {});
+app.get("/extrato", (req, res) => {});
 
 app.listen(process.env.PORT, () => console.log("Servidor online"));
