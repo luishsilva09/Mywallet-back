a80bffd55f54822dc21eb75d1229024acc2f462a
luis <luishsilva09@gmail.com>
2022-06-29 17:34:54 -0300

    configuração

--- app.js
+++ app.js
@@ -6,17 +6,18 @@ import joi from "joi";
 
 dotenv.config();
 
-const MAX_TEMPO_INATIVO = 10000;
-let listaParticipantes = [];
-
 const client = new MongoClient(process.env.URL_CONECT_MONGO);
 let db;
 client.connect().then(() => {
-  db = client.db("batepapo-uol-api");
+  db = client.db("mywallet-api");
 });
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.post("/cadastro", (req, res) => {
+  res.send("foii");
+});
 
 app.listen(process.env.PORT, () => console.log("Servidor online"));
