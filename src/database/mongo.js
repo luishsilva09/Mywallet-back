34976e74b404d04db3d4b8adb971dfe43ce63a9d
luis <luishsilva09@gmail.com>
2022-07-01 20:54:12 -0300

    chore: adjust code for deploy on heroku

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
-const client = new MongoClient(process.env.URL_CONECT_MONGO);
+const client = new MongoClient(process.env.MONGO_URI);
 await client.connect();
 const db = client.db("mywallet-api");
 
