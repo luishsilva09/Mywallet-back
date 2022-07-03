a89dadd300bd54bca4929ef5ab08ac905141d05e
luis <luishsilva09@gmail.com>
2022-07-03 19:13:24 -0300

    colocando online

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { cadastro, login } from "../controllers/authController.js";
+import cors from "cors";
 
 const authRoutes = express.Router();
+authRoutes.use(cors());
 
 authRoutes.post("/cadastro", cadastro);
 authRoutes.post("/login", login);
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { entrada, saida, extrato } from "../controllers/userController.js";
 import validDataAdd from "../middlewares/validDataAdd.js";
+import cors from "cors";
 
 const userRoutes = express.Router();
+userRoutes.use(cors());
 
 userRoutes.post("/entrada", validDataAdd, entrada);
 userRoutes.post("/saida", saida);
