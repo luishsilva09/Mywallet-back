7db735fadfb9e337882efabe3cf8f56d099a8c5f
luis <luishsilva09@gmail.com>
2022-07-03 19:57:04 -0300

    corrigindo erro cors

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,7 @@
 import express from "express";
 import { cadastro, login } from "../controllers/authController.js";
-import cors from "cors";
 
 const authRoutes = express.Router();
-authRoutes.use(cors());
 
 authRoutes.post("/cadastro", cadastro);
 authRoutes.post("/login", login);
