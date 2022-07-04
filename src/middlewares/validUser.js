f6425c01ae96ee51c42812ebacd20aafeac7d96e
luis <luishsilva09@gmail.com>
2022-07-04 15:18:56 -0300

    validar com midlleware

--- src/middlewares/validUser.js
+++ src/middlewares/validUser.js
@@ -1,5 +1,5 @@
 import db from "../database/mongo.js";
-export default async function validUser(req, res) {
+export default async function validUser(req, res, next) {
   const { authorization } = req.headers;
 
   const token = authorization?.replace("Bearer ", "");
