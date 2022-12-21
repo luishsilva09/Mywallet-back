ba374fac6937f2f8d006adab7e93d00028651204
Luís Henrique <luishsilva09@gmail.com>
2022-12-21 19:17:38 -0300

    change function names

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,6 @@
-import bcrypt from "bcrypt";
-import joi from "joi";
-import db from "../database/mongo.js";
-import { v4 as uuid } from "uuid";
-import { ObjectId } from "mongodb";
 import * as authService from "../service/authService.js";
 
-export async function cadastro(req, res) {
+export async function signup(req, res) {
   try {
     const result = await authService.signup(req.body);
     res.status(result.code).send(result.message);
@@ -14,7 +9,7 @@ export async function cadastro(req, res) {
   }
 }
 
-export async function login(req, res) {
+export async function signin(req, res) {
   try {
     const { email, password } = req.body;
     const result = await authService.singin(email, password);
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,11 @@
-import express from "express";
-import { cadastro, login } from "../controllers/authController.js";
+import { Router } from "express";
+import { signup, signin } from "../controllers/authController.js";
 import { schemaValidate } from "../middlewares/schemaValidate.js";
 import * as authSchema from "../schemas/authSchema.js";
 
-const authRoutes = express.Router();
+const authRoutes = Router();
 
-authRoutes.post("/signup", schemaValidate(authSchema.newUserSchema), cadastro);
-authRoutes.post("/signin", login);
+authRoutes.post("/signup", schemaValidate(authSchema.newUserSchema), signup);
+authRoutes.post("/signin", signin);
 
 export default authRoutes;
